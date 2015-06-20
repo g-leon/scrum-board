ed52cc697ec052a2720a5e5926fb9b89d778895f
Leon Guianu <guianu.leon@gmail.com>
2015-06-20 20:49:08 +0300

    Add sprint view set

--- board/views.py
+++ board/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Sprint
+from .serializers import SprintSerializer
 
-# Create your views here.
+
+class SprintViewSet(viewsets.ModelViewSet):
+    """API endpoint for listing and creating sprints."""
+
+    queryset = Sprint.objects.order_by('end')
+    serializer_class = SprintSerializer
