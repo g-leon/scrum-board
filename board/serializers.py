bcef742128bad846091580e5eb6ce2fe6eeea4d3
Leon Guianu <guianu.leon@gmail.com>
2015-06-20 21:21:53 +0300

    Add serializer for User model
    The User model might me swapped out for another and the
    intent of the application is to be as reusable as possible. To
    create this switch in a clean way get_user_model Django utility
    is used.
    
    The serializer assumes that if a custom User model is used, then
    it extends from django.contrib.auth.models.CustomUser, which
    will always have a USERNAME_FIELD attribute, get_full_name
    metod, and is_active attribute.

--- board/serializers.py
+++ board/serializers.py
@@ -1,8 +1,10 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
 from rest_framework import serializers
 
 from .models import Sprint, Task
 
+User = get_user_model()
 
 class SprintSerializer(serializers.ModelSerializer):
 
@@ -24,3 +26,12 @@ class TaskSerializer(serializers.ModelSerializer):
 
     def get_status_display(self, obj):
         return obj.get_status_display()
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    full_name = serializers.CharField(source='get_full_name', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active', )
