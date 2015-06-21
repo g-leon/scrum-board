1fdfa03e2660232abdc69ac001484eb8fd285aec
Leon Guianu <guianu.leon@gmail.com>
2015-06-21 17:39:12 +0300

    Change assigned attribute from task to reference users by name
     Users are referenced by the pk, while the rest of the API uses
     the username as a unique identifier. This can be changed by
     changing the field used by the underlying ModelChoiceField.

--- board/forms.py
+++ board/forms.py
@@ -1,7 +1,11 @@
 import django_filters
 
+from django.contrib.auth import get_user_model
+
 from .models import Task
 
+
+User = get_user_model()
 
 class NullFilter(django_filters.BooleanFilter):
     """Filter on a field set as null or not."""
@@ -19,3 +23,7 @@ class TaskFilter(django_filters.FilterSet):
     class Meta:
         model = Task
         fields = ('sprint', 'status', 'assigned', 'backlog', )
+
+    def __init__(self, *args, **kwargs):
+        super(TaskFilter, self).__init__(*args, **kwargs)
+        self.filters['assigned'].extra.update({'to_field_name': User.USERNAME_FIELD})
