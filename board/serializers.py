c3eb3a1d64b1e6af7c635e5e276b07c00f0db527
Leon Guianu <guianu.leon@gmail.com>
2015-06-23 19:58:13 +0300

    Add validation for tasks
    validate_sprints ensures that the sprint is not changed after the task
    is completed and that tasks are not assigned to sprints that have
    already been completed.
    
    validate ensures that tasks fields are correct

--- board/serializers.py
+++ board/serializers.py
@@ -60,6 +60,38 @@ class TaskSerializer(serializers.ModelSerializer):
                 links['assigned'] = reverse('user-detail', kwargs={User.USERNAME_FIELD: obj.assigned}, request=request)
         return links
 
+    def validate_sprint(self, value):
+        if self.instance and self.instance.pk:
+            if value != self.instance.sprint:
+                if self.instance.status == Task.STATUS_DONE:
+                    msg = _('Cannot change the sprint of a completed task.')
+                    raise serializers.ValidationError(msg)
+                if value and value.end < date.today():
+                    msg = _('Cannot assign tasks to past sprints.')
+                    raise serializers.ValidationError(msg)
+        else:
+            if value and value.end < date.today():
+                msg = _('Cannot add tasks to past sprints.')
+                raise serializers.ValidationError(msg)
+        return value
+
+    def validate(self, attrs):
+        sprint = attrs.get('sprint')
+        status = int(attrs.get('status'))
+        started = attrs.get('started')
+        completed = attrs.get('completed')
+
+        if not sprint and status != Task.STATUS_TODO:
+            msg = _('Backlog tasks must have "Not Started" status.')
+            raise serializers.ValidationError(msg)
+        if started and status == Task.STATUS_TODO:
+            msg = _('Started date cannot be set for not started tasks.')
+            raise serializers.ValidationError(msg)
+        if completed and status != Task.STATUS_DONE:
+            msg = _('Completed date cannot be set for uncompleted tasks.')
+            raise serializers.ValidationError(msg)
+        return attrs
+
 
 class UserSerializer(serializers.ModelSerializer):
     full_name = serializers.CharField(source='get_full_name', read_only=True)
