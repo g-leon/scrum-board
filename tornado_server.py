ea414044ae4be410259fd29e710ce69fd6d3d2e8
Leon Guianu <guianu.leon@gmail.com>
2015-06-24 21:20:12 +0300

    Add endpoint in Tornado server under task to receive updates
    Django can now push updates about new tasks or updated tasks.

--- tornado_server.py
+++ tornado_server.py
@@ -1,13 +1,14 @@
 import logging
 import signal
 import time
-from collections import defaultdict
 import urlparse
+import json
+from collections import defaultdict
 
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from tornado.options import define, parse_command_line, options
-from tornado.web import Application
+from tornado.web import Application, RequestHandler
 from tornado.websocket import WebSocketHandler, WebSocketClosedError
 
 define('debug', default=False, type=bool, help='Run in debug mode')
@@ -38,10 +39,33 @@ class SprintHandler(WebSocketHandler):
         self.application.remove_subscriber(self.sprint, self)
 
 
+class UpdateHandler(RequestHandler):
+    """Handle updates from the Django application."""
+
+    def post(self, model, pk):
+        self._broadcast(model, pk, 'add')
+
+    def put(self, model, pk):
+        self._broadcast(model, pk, 'update')
+
+    def delete(self, model, pk):
+        self._broadcast(model, pk, 'remove')
+
+    def _broadcast(self, model, pk, action):
+        message = json.dumps({
+            'model': model,
+            'id': pk,
+            'action': action,
+        })
+        self.application.broadcast(message)
+        self.write("Ok")
+
+
 class ScrumApplication(Application):
     def __init__(self, **kwargs):
         routes = [
             (r'/(?P<sprint>[0-9]+)', SprintHandler),
+            (r'/(?P<model>task|sprint|user)/(?P<pk>[0-9]+)', UpdateHandler),
         ]
         super(ScrumApplication, self).__init__(routes, **kwargs)
         self.subscriptions = defaultdict(list)
